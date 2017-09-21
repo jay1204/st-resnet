484eb17c86afdaa3b1df2756ee2f27d93a63f51a
jay1204 <zchen22@ncsu.edu>
2017-09-20 23:40:19 -0400

    fix bug

--- train_image.py
+++ train_image.py
@@ -12,14 +12,14 @@ def main():
     ctx = mx.gpu(0)
     classes_labels, train_videos_classes, test_videos_classes = get_ucf101_split(ucf.split_dir, ucf.split_id)
 
-    videos = list(test_videos_classes.keys())
-    sample_videos= random.sample(videos, 500)
-    test_videos_classes_samples = {}
-    for video in sample_videos:
-        test_videos_classes_samples[video] = test_videos_classes[video]
+    #videos = list(test_videos_classes.keys())
+    #sample_videos= random.sample(videos, 500)
+    #test_videos_classes_samples = {}
+    #for video in sample_videos:
+    #    test_videos_classes_samples[video] = test_videos_classes[video]
 
     cm = CNN_Image(model_params=resnet_50, data_params=ucf.image, train_params=train_image, test_params=test_image,
-                   train_videos_classes=train_videos_classes, test_videos_classes=test_videos_classes_samples,
+                   train_videos_classes=train_videos_classes, test_videos_classes=test_videos_classes,
                    classes_labels=classes_labels, num_classes=ucf.num_classes, ctx=ctx)
     cm.train()
     return
