ad7d2a793558a42d2e43f6bc38036ff10cd0439c
jay1204 <zchen22@ncsu.edu>
2017-09-24 23:52:28 -0400

    change pretrained model to vgg 16

--- model/convnet.py
+++ model/convnet.py
@@ -74,7 +74,7 @@ class ConvNet(object):
         elif self.model_params.name=='vgg16':
             all_layers = symbol.get_internals()
             print all_layers
-            net = all_layers['relu6']
+            net = all_layers['relu6_output']
             net = mx.symbol.Dropout(net, p=self.train_params.drop_out)
             net = mx.symbol.FullyConnected(data=net, num_hidden=4096, name='fc7')
             net = mx.symbol.Activation(name='relu7', data=net, act_type='relu')
