dac37b4f8a506663d2b40e7c5c466b9b2a6d47da
jay1204 <zchen22@ncsu.edu>
2017-09-24 22:32:47 -0400

    add one more fully connected layer and dropout layer

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 
 # set up logger
-logging.basicConfig(filename='experiment.log')
+logging.basicConfig(filename='experiment1.log')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
