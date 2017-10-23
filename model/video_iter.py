27391505bbcd501b5ce3a185383df79c337103cd
jay1204 <zchen22@ncsu.edu>
2017-10-23 16:28:36 -0400

    create multiple threads to read images

--- model/video_iter.py
+++ model/video_iter.py
@@ -16,7 +16,7 @@ class VideoIter(mx.io.DataIter):
     """
     def __init__(self, batch_size, data_shape, data_dir, videos_classes, classes_labels, ctx=None, data_name='data',
                  label_name='label', mode='train', augmentation=None, clip_per_video=1, frame_per_clip=1, lst_dict=None,
-                 record=None, multiple_processes=4, multiple_threads=8):
+                 record=None, multiple_processes=4, multiple_threads=1):
         """
 
         :param batch_size:
