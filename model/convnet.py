ee7999b874e13c3b257faa1a1e7918184b23d73e
jay1204 <zchen22@ncsu.edu>
2017-10-25 19:25:51 -0400

    change read image from rgb to greyscale

--- model/convnet.py
+++ model/convnet.py
@@ -110,7 +110,7 @@ class ConvNet(object):
             net = mx.symbol.FullyConnected(data=net, num_hidden=self.num_classes, name='fc1')
             new_symbol = mx.symbol.SoftmaxOutput(data=net, name='softmax')
             new_arg_params = dict({k: arg_params[k] for k in arg_params if 'fc1' not in k})
-            print new_arg_params['bn_data_gamma'].shape
+            print new_arg_params['bn_data_gamma']
             new_arg_params['bn_data_gamma'] = mx.ndarray.repeat(
                 new_arg_params['bn_data_gamma'],repeats=self.train_params.frame_per_clip * len(self.data_params.dir))
             new_arg_params['bn_data_beta'] = mx.ndarray.repeat(
