fe034b64ed74558d164425d2d55eef542ef9420f
jay1204 <zchen22@ncsu.edu>
2017-12-26 21:46:00 -0500

    do not use the self designed resnet

--- model/convnet.py
+++ model/convnet.py
@@ -307,7 +307,7 @@ class ConvNet(object):
             momentum = "0.9"
 
         for param in json_file['nodes']:
-            if param['op'] == 'BatchNorm' and self.mode == 'temporal' and param['name'] == 'bn_data':
+            if (param['op'] == 'BatchNorm') and (self.mode == 'temporal') and (param['name'] == 'bn_data'):
                 param['attr']['use_global_stats'] = "False"
                 param['attr']['momentum'] = "0.9"
             elif param['op'] == 'BatchNorm':
